08dae058fb337db5759960eefaa11be0c15b2bbd
@cai_yong_jie <1969764749@qq.com>
2023-12-05 14:07:49 +0800

    Configure the seo information of the head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,4 +15,33 @@ export default defineNuxtConfig({
       },
     },
   },
+  app: {
+    // 给所有页面的head添加seo信息
+    head: {
+      title: "Nuxt3-OPPO",
+      meta: [
+        //安全政策，需要ssl证书
+        // {"http-equiv":"Content-Security-Policy",content:"upgrade-insecure-requests"},
+        //<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        {
+          name: "description",
+          content:
+            "OPPO专区，官方正品，最新最全的OPPO手机产品以及配件在线抢购！",
+        },
+        {
+          name: "keywords",
+          content: "OPPO商城，OPPO专区, OPPO手机，OPPO配件，OPPO, OPPO官网商城",
+        },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ],
+      link: [
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        // { rel: "stylesheet", href: "https://liujun.css" }
+      ],
+      // add head
+      // script: [{ src: "https://liujun.js" }],
+      noscript: [{ children: "Javascript is required" }],
+      //   bodyAttrs:{style:"margin:0"}
+    },
+  },
 });
