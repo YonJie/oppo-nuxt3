91465f2257f40e9933dc7cdf2a4d3ef679624ad1
@cai_yong_jie <1969764749@qq.com>
2023-12-05 14:09:45 +0800

    Configuring 404 Pages

---
+++ pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="page-not-fund">
+    <h3>page-not-fund: 404</h3>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.page-not-fund {
+  color: red;
+}
+</style>
