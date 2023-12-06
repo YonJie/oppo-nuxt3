0e24240a79802a585ad0d0c73134a3140704742a
@cai_yong_jie <1969764749@qq.com>
2023-12-06 10:01:04 +0800

    Implement the category component

--- pages/index.vue
+++ pages/index.vue
@@ -2,15 +2,23 @@
   <div class="home">
     <div class="wrapper">
       <swiper :banners="banners"></swiper>
-      <tab-category></tab-category>
+      <tab-category
+        :categorys="categorys"
+        @item-click="handleItemClick"
+      ></tab-category>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import { useHomeStore } from "~/store/home";
 import { storeToRefs } from "pinia";
+import type { ICategory } from "~/store/home";
 
 const homeStore = useHomeStore();
-const { banners } = storeToRefs(homeStore);
+const { banners, categorys } = storeToRefs(homeStore);
+
+function handleItemClick(item: ICategory) {
+  // console.log(item.title);
+}
 </script>
 <style lang="scss"></style>
